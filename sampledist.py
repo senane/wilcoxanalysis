8a14156014616f2c4e16d238536378403b1b5dff
Senan <supersenan@gmail.com>
2018-07-12 19:01:51 -0400

    second try

--- sampledist.py
+++ sampledist.py
@@ -2,11 +2,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.Series(data=[4, 5, 6, 9, 56, 45, 68, 28, 85, 143, 240, 280])
+df = pd.Series(data=[4, 5, 6, 7, 8])
 
 plt.hist(df, bins = range(min
                           (df), max(df)+5, 5))
-plt.title("Cash Sz Histogram (" + str(np.sum(df)) + " sz from " + str(df.size) + " rats)")
+plt.title("Sample Sz Histogram (" + str(np.sum(df)) + " sz from " + str(df.size) + " rats)")
 plt.xlabel("# of Seizures")
 plt.ylabel("# of Rats")
 
